506f80da54828545bc3b79f8b2fad924427e8769
Nikolay <ndikanskiy@gmail.com>
2022-06-03 09:21:28 -0700

    Add homework for lesson 1

---
+++ HomeworkLesson1_1.java
@@ -0,0 +1,20 @@
+public class Lesson1Homework1_1 {
+
+	public static void main(String... args) {
+		
+		System.out.println("H");
+		System.out.println("e");
+		System.out.println("l");
+		System.out.println("l");
+		System.out.println("o");
+		System.out.println("");
+		System.out.println("W");
+		System.out.println("o");
+		System.out.println("r");
+		System.out.println("l");
+		System.out.println("d");
+		System.out.println("!");
+
+	}
+
+}
---
+++ HomeworkLesson1_2.java
@@ -0,0 +1,9 @@
+public class Lesson1Homework1_2 {
+
+	public static void main(String... args) {
+		
+		System.out.println("0 1 2 3 4 5 6 7 8 9");
+	
+	}
+
+}
