ab4bd7df60e1a41b1f92cebf0cee279e9e6b10da
Nikolay <ndikanskiy@gmail.com>
2022-06-07 09:21:42 -0700

    Add homework for lesson 2

---
+++ HomeworkLesson2.java
@@ -0,0 +1,13 @@
+public class HomeworkLesson2 {
+
+	public static void main(String... args) {
+
+		long salary = 75_000;
+		double taxes = 0.13;
+
+		System.out.println((long)(salary - salary * taxes));
+
+	}
+
+}
+
